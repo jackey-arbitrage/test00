3290e9f7bb8a868b96cc8631762c4d24a8031cc4
CentOS-6.5 <jackey@localhost.localdomain>
2014-11-09 12:43:26 +0800

    update feature 2

--- src/hello.java
+++ src/hello.java
@@ -3,7 +3,8 @@ public class hello
   public hello()
   {
     System.out.println("hello world! 12345");
-    System.out.println("feature 2 added!");
+    System.out.println("feature 2 added!);
+
   }
 
   public static void main(String[] argv)
